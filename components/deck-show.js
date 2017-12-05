718e61f7e5e5a21778374b9eb86f91c60a92122f
Torey Hickman <torey@toreyhickman.com>
2017-12-04 19:59:05 -0500

    feat: Show title and card count on deck page

--- components/deck-show.js
+++ components/deck-show.js
@@ -1,8 +1,49 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { Component } from "react"
+import { View, Text, StyleSheet } from "react-native"
+import { connect } from "react-redux"
+import { mediumgray } from "../utils/colors"
 
-export default () => (
-  <View>
-    <Text>Deck!</Text>
-  </View>
-)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: 40,
+    paddingBottom: 80
+  },
+
+  deckDetails: {
+    alignItems: "center"
+  },
+
+  deckHeading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+
+  deckCardCount: {
+    color: mediumgray
+  }
+})
+
+class DeckShow extends Component {
+  render() {
+    const { title, cards } = this.props.deck
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.deckDetails}>
+          <Text style={styles.deckHeading}>{title}</Text>
+          <Text style={styles.deckCardCount}>{cards.length} cards</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = ({decks}, ownProps) => ({
+  deck: decks.find(deck => deck.title === ownProps.navigation.state.params.title)
+})
+
+export default connect(mapStateToProps)(DeckShow)
